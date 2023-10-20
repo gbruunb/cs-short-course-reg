869b3b98e17609cbb995f0adfb9aacdd57e5c836
GBru <unnameboy2546@hotmail.com>
2023-10-21 03:25:48 +0700

    add register and sign-in page

--- controllers/MainController.js
+++ controllers/MainController.js
@@ -1,5 +1,38 @@
-module.exports = {
-    home: (req, res) => {
-        res.render('home')
-    }
+const { connection } = require("./DBConnection");
+
+
+function home(req, res) {
+    res.render('home')
 }
+
+function signInPage(req, res) {
+    res.render('signin')
+}
+
+function registerPage(req, res) {
+    res.render('register')
+}
+
+function register(req, res) {
+    const { fname, lname, email, student_id } = req.body;
+    try {
+      connection.query(
+        "INSERT INTO user(fname, lname, email, student_id) VALUE(?,?,?,?)",
+        [fname, lname, email, student_id],
+        (err, result, fields) => {
+          if (err) {
+            console.log("insert error");
+            console.log(err);
+            return res.status(400).send();
+          }
+          return res
+            .status(201)
+            .json({ message: "New user created successfully" });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send();
+    }
+  }
+module.exports = { home, signInPage, registerPage, register }
--- routes/router.js
+++ routes/router.js
@@ -7,10 +7,14 @@ const StudentController = require('../controllers/StudentController')
 
 
 router.get('/',MainController.home)
+router.get('/signin',MainController.signInPage)
+router.get('/register',MainController.registerPage)
+
 router.get('/create-course-form',LecturerController.createCourseForm)
 router.get('/course-list',StudentController.showCourseList)
 router.get('/edit-course/:id',LecturerController.editCourseForm)
 
+router.post('/register',MainController.register)
 
 router.post('/add-course',LecturerController.addCourse)
 router.post('/update-course/:id',LecturerController.editCourse)
