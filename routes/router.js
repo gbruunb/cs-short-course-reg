465b94ba3787ec2e644f5f9a5015192b1619c3e0
GBru <unnameboy2546@hotmail.com>
2023-10-20 14:30:03 +0700

    add controller

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/',(req,res) => {
-    res.render('home') 
-})
+const MainController = require('../controllers/MainController')
+
+router.get('/',MainController.get)
 
 module.exports = router
