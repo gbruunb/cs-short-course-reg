1b7006803a7d85f577be01ec5ad190fa7eee8d12
GBru <unnameboy2546@hotmail.com>
2023-10-20 23:46:00 +0700

    can update

--- controllers/LecturerController.js
+++ controllers/LecturerController.js
@@ -26,5 +26,44 @@ function addCourse(req, res) {
   }
 }
 
+function editCourseForm(req,res) {
+    const id = req.params.id
+    try {
+        connection.query("SELECT * FROM course WHERE id = ?",[id], (err, result, fields) => {
+            if(err){
+                console.log(err);
+                return res.status(400).send
+            }
+            res.render('lecturer/edit-course', { details: result })
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).send
+    }
+  }
 
-module.exports = { createCourseForm, addCourse };
+function editCourse(req, res) {
+    const id = req.params.id
+    const { name, course_name, description } = req.body;
+    try {
+      connection.query(
+        "UPDATE course SET name = ?, course_name = ?, description = ? WHERE id = ?",
+        [name, course_name, description, id],
+        (err, result, fields) => {
+          if (err) {
+            console.log("insert error");
+            return res.status(400).send();
+          }
+          return res
+            .status(201)
+            .json({ message: "New user created successfully" });
+        }
+      );
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send();
+    }
+  }
+
+
+module.exports = { createCourseForm, addCourse, editCourseForm, editCourse };
--- routes/router.js
+++ routes/router.js
@@ -9,7 +9,12 @@ const StudentController = require('../controllers/StudentController')
 router.get('/',MainController.home)
 router.get('/create-course-form',LecturerController.createCourseForm)
 router.get('/course-list',StudentController.showCourseList)
+router.get('/edit-course/:id',LecturerController.editCourseForm)
+
 
 router.post('/add-course',LecturerController.addCourse)
+router.post('/update-course/:id',LecturerController.editCourse)
+// router.post('/delete-course/:id',LecturerController.deleteCourse)
+
 
 module.exports = router
