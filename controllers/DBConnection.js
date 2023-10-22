fb3582ad18e34b32a14e136542e024a153d62f27
NONGAOM <aomnak_aj@hotmail.co.th>
2023-10-22 23:37:34 +0700

    home & signin done

--- controllers/DBConnection.js
+++ controllers/DBConnection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'cs_short_course',
     port:'3306'
 })
